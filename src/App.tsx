e4f074c4a30f70767cc439942995aa8e9d4013ff
gaet9 <gaetan.delorgeril@gmail.com>
2026-03-31 16:45:16 -0300

    speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { ThemeProvider } from "@/components/ui/theme-provider.tsx";
 import BookLanding from "./pages/BookLanding";
 import NotFound from "./pages/NotFound";
@@ -23,6 +24,7 @@ const App = () => (
                         <Route path='*' element={<NotFound />} />
                     </Routes>
                     <Analytics />
+                    <SpeedInsights />
                 </BrowserRouter>
             </ThemeProvider>
         </TooltipProvider>
